963a0817dc092cfd10a84cbd6a96009c1921ae99
nmzik <Nmzik@mail.ru>
2026-09-24 03:04:07 +0400

    Bound uniformized image keys by guarded material mask
    Co-authored-by: TarkusR <90155059+TarkusR@users.noreply.github.com>

--- src/graphics/shader/recompiler/ir/ShaderIR.h
+++ src/graphics/shader/recompiler/ir/ShaderIR.h
@@ -465,6 +465,7 @@ struct DescriptorSource {
 		uint32_t selector_offset = 0;
 		uint32_t table_offset    = 0;
 		Value    key_count;
+		Value    selector_mask;
 
 		bool operator==(const IndirectImage& other) const = default;
 	};
@@ -540,6 +541,7 @@ struct ResourcePlan {
 	std::vector<SrtRead>                srt_reads;
 	std::vector<uint8_t>                clean_flat_slots;
 	bool                                requires_specialization_memory = false;
+	bool                                has_address_writes = false;
 	bool                                srt_plan_complete          = false;
 	bool                                resource_tracking_complete = false;
 	ShaderInfo                          info;
@@ -552,6 +554,7 @@ struct ResourcePlan {
 	mutable std::vector<uint8_t>            visited_blocks;
 	mutable std::vector<uint32_t>           pending_blocks;
 	mutable std::vector<uint32_t>           material_keys;
+	mutable std::vector<std::pair<uint64_t, uint64_t>> specialization_reads;
 };
 
 struct Program: ResourcePlan {
